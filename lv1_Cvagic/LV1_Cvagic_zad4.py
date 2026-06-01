c8a5f31dafaa3ca3b23f086a571a89da0061241b
ncvagic <noa.cvagic@gmail.com>
2026-06-01 20:35:47 +0200

    Update LV1_Cvagic_zad4.py

--- lv1_Cvagic/LV1_Cvagic_zad4.py
+++ lv1_Cvagic/LV1_Cvagic_zad4.py
@@ -1,3 +1,16 @@
-print("Unesi ime tekstualne datoteke:")
-ime_datoteke = input()
+ime = input("Ime datoteke: ")
 
+fhand = open(ime)
+
+suma = 0
+brojac = 0
+
+for line in fhand:
+    if line.startswith("X-DSPAM-Confidence:"):
+        broj = float(line.split(":")[1])
+        suma += broj
+        brojac += 1
+
+fhand.close()
+
+print("Average X-DSPAM-Confidence:", suma / brojac)
